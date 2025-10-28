b432614a864f6f2040c747ca28fd7d680cb024ba
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 12:31:21 -0400

    Concat

---
+++ Javascript/Assignment/01_Concattenate two arrays[1,2,3] and [4,5,6] into a single array.js
@@ -0,0 +1,5 @@
+///Concatenate two arrays [1,2,3] and [4,5,6] into a single array.
+ var Array = [1,2,3];
+ var NewArray =[5,6,7];
+var  X = Array.concat(NewArray);
+ console.log(X);
---
+++ Javascript/Assignment/02_Concatenate an array of fruits and vegetables.js
@@ -0,0 +1,6 @@
+//Concatenate an array of fruits and vegetables.
+
+var Fruits = ["Mango","Banana","Apple","Oragne"];
+var vegetables = ["Potato","Tomato","carrot","Onion"];
+var NewArray = Fruits.concat(vegetables);
+console.log(NewArray);
