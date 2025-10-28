5402f6455137708b4e6bbc8a4288ed74a875c805
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 13:34:35 -0400

    Join

---
+++ Javascript/Assignment/Join/01_Convert an array of Characters.js
@@ -0,0 +1,5 @@
+//Convert an array of characters ['J','S'] into a string "JS".
+
+ var Array = ["J","S"];
+ const  a = Array.join("");
+ console.log(a);
---
+++ Javascript/Assignment/Join/02_Create an array of words and join them with a space.js
@@ -0,0 +1,6 @@
+//Create an array of words and join them with a space.
+
+
+let Array = ["Harry","Potter","Karad","Pune"];
+let a = Array.join(" ");
+console.log(a);
