26cbe0625c863c7c2a4b25e79631018984452bcf
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 13:36:20 -0400

    Reverse

---
+++ Javascript/Assignment/Reverse/01_Create an array [1,2,3,4,5] and reverse it.js
@@ -0,0 +1,5 @@
+///Create an array [1,2,3,4,5] and reverse it.
+
+var Array = [1,2,3,4,5];
+Array.reverse();
+console.log(Array);
---
+++ Javascript/Assignment/Reverse/02_Revrese an array of Character and join them into a string.js
@@ -0,0 +1,4 @@
+//Reverse an array of characters and join them into a string.
+var StudentName = ["Harry","Ram","Dev","Krishna"];
+StudentName.reverse();
+console.log(StudentName);
