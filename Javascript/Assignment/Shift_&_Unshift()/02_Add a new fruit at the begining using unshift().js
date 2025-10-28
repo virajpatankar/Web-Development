09145f890ce4a6c2cdcc0752f4ba8da6f04d310d
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 13:38:01 -0400

    Shift_&_Unshift

---
+++ Javascript/Assignment/Shift_&_Unshift()/02_Add a new fruit at the begining using unshift().js
@@ -0,0 +1,6 @@
+// 02_Add_a_new_fruit_at_the_beginning_using_unshift().js
+
+
+var Fruits = ["Mango","Banana","Apple","Grapes","Strawberries"];
+Fruits.unshift("Cocount");
+console.log(Fruits);
---
+++ Javascript/Assignment/Shift_&_Unshift()/Create An Array Of Fruit Remove The First Fruit Using Shift().js
@@ -0,0 +1,5 @@
+//////Create_An_Array_Of_Fruit_Remove_The_First_Fruit_Using_Shift().js
+
+var Fruits = ["Mango","Banana","Graphs","Apple","Strawberries"];
+Fruits.shift();
+console.log(Fruits);
