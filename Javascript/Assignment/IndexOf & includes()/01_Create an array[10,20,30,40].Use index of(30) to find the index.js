3b97e8ed757b9a3ebb23405869d147f55b2afd1d
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 12:59:50 -0400

    IndexOf & includes()

---
+++ Javascript/Assignment/IndexOf & includes()/01_Create an array[10,20,30,40].Use index of(30) to find the index.js
@@ -0,0 +1,5 @@
+//Create an array [10,20,30,40]. Use indexOf(30) to find the index.
+ var No = [10,20,30,40];
+ let index = No.indexOf(30);
+
+ console.log(index);
---
+++ Javascript/Assignment/IndexOf & includes()/02_Check if 50 is Present Using includes().js
@@ -0,0 +1,6 @@
+///Check if 50 is present using includes().
+
+let Number = [10,20,30,40,500];
+let ispresent = Number.includes(50);
+
+console.log(ispresent);
