411321269622c8ffe552780e619d336277bd8325
virajpatankar <virajpatankar70@gmail.com>
2025-10-28 12:49:01 -0400

    Delete Javascript/Assignment/02_Concatenate an array of fruits and vegetables.js

--- Javascript/Assignment/02_Concatenate an array of fruits and vegetables.js
+++
@@ -1,6 +0,0 @@
-//Concatenate an array of fruits and vegetables.
-
-var Fruits = ["Mango","Banana","Apple","Oragne"];
-var vegetables = ["Potato","Tomato","carrot","Onion"];
-var NewArray = Fruits.concat(vegetables);
-console.log(NewArray);
