6a8aed2f6f56df5372ee48c0408906794d3d9537
virajpatankar <virajpatankar70@gmail.com>
2025-10-29 02:39:54 -0400

    Sort

---
+++ Javascript/Assignment/Sort/01_Create an array of numbers and sort them in ascending order.js
@@ -0,0 +1,7 @@
+//Create an array of numbers and sort them in ascending order.
+
+var numbers = [80,17,89,67,144,78];
+numbers.sort(function(a,b){
+ return a-b;
+});
+console.log(numbers);
---
+++ Javascript/Assignment/Sort/02_Sort an array of strings alphabetically.js
@@ -0,0 +1,7 @@
+//Sort an array of strings alphabetically.
+
+
+
+var StudentNames = ["Dhiraj","Rudra","Shambu","Parth"];
+StudentNames.sort();
+console.log(StudentNames);
