03dd0439f8cca0e6a851e23c3063f81fbead63e7
virajpatankar <virajpatankar70@gmail.com>
2025-10-29 02:31:40 -0400

    Slice

---
+++ Javascript/Assignment/Slice/01_Create_an_Array of 10 numbers. Extract the first 5 numbers using slice().js
@@ -0,0 +1,7 @@
+//01_Create_an_array_of_10_numbers. Extract thefirst_5_numbers_using_slice().js
+
+
+var Value = [1,2,3,4,5,6,7,8,9,10];
+//var X = Value.slice(0,5);
+var X = Value.slice(5,10);
+console.log(X);
---
+++ Javascript/Assignment/Slice/Extract the last 3 number using slice().js
@@ -0,0 +1,6 @@
+//Extract_the_last_3_numbers_using_slice()..js
+
+var Values = [1,2,3,4,5,6,7,8,9,10];
+// var X = Values.slice(7);
+var X = Values.slice(0,7);
+console.log(X);
