842323f075a9171fe7fae60f2093310532ea9e95
virajpatankar <virajpatankar70@gmail.com>
2025-10-29 02:41:24 -0400

    Splice

---
+++ Javascript/Assignment/Splice/02 Insert two numbers at index 1 sing splice().js
@@ -0,0 +1,6 @@
+// Insert two numbers at index 1 using splice().
+
+var Array = [10,20,30,40,50];
+Array.splice(1,1,400,700);
+//Array.splice(1,0,400);
+console.log(Array);
---
+++ Javascript/Assignment/Splice/Create an array [10,20,30,40,50]. Remove the element at index 2 using splice().js
@@ -0,0 +1,5 @@
+//Create an array [10,20,30,40,50]. Remove the element at index 2 using splice().
+
+var Array = [10,20,30,40,50];
+Array.splice(2,1);
+console.log(Array);
