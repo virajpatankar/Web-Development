0c00cbc0a357e79b86a096d90a52169de9c638df
virajpatankar <virajpatankar70@gmail.com>
2025-11-01 05:01:35 -0400

    Demo

---
+++ Javascript/Demo/Index.js
@@ -0,0 +1,11 @@
+console.log("Welcome To Fork Infosystem!");
+console.log("I love Fork Infosystem");
+
+name = "Tony Stark";
+console.log("Tony stark")
+age = 24;
+console.log("24");
+City = "Patan";
+console.log("Patan");
+College = "Sadguru ghadge maharaj college karad";
+console.log("Sadguru ghadge maharaj college karad");   
