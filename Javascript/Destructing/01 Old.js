fd7ee5f039d0904dee1e218ad5e95ad2919f62e9
virajpatankar <virajpatankar70@gmail.com>
2025-11-01 05:07:16 -0400

    Destructing

---
+++ Javascript/Destructing/01 Old.js
@@ -0,0 +1,14 @@
+let arr = [10,20,30,40,50];
+let arr1 = arr[0];
+let arr2 = arr[1];
+let arr3 = arr[2];
+let arr4 = arr[3];
+let arr5 = arr[4];
+
+console.log("Whole Array",arr);
+console.log("Bites of Arrayu",arr1);
+console.log("Bites of Arrayu",arr2);
+console.log("Bites of Arrayu",arr3);
+console.log("Bites of Arrayu",arr4);
+console.log("Bites of Arrayu",arr5);
+
---
+++ Javascript/Destructing/02.new.js
@@ -0,0 +1,9 @@
+let arr = [10,20,30,40,50];
+let[a,b,c,d,e] = arr;
+
+console.log("Whole Array",arr);
+console.log("Whole Array",a);
+console.log("Whole Array",b);
+console.log("Whole Array",c);
+console.log("Whole Array",d);
+console.log("Whole Array",e);
